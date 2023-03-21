8c96f09b77c2068b162ff536123ce95f456765d2
Luis Guilherme <luislebert01@outlook.com>
2023-03-21 14:33:21 -0300

    Opções reorganizadas

--- FileManipulation/FileManipulator.cs
+++ FileManipulation/FileManipulator.cs
@@ -1,4 +1,6 @@
-﻿namespace FileManipulation
+﻿using System.Runtime.CompilerServices;
+
+namespace FileManipulation
 {
     internal class FileManipulator
     {
@@ -29,6 +31,21 @@
             sw.Close();
 
             return true;
+        }
+
+        public string[] ReadFileLines(int l)
+        {
+            if (File.Exists(FullPath)) sr = new StreamReader(FullPath);
+            else return null;
+
+            string[] fullText = new string[l];
+
+            for (int i = 0; i < l; i++)
+            {
+                fullText[i] = sr.ReadLine();
+            }
+            sr.Close();
+            return fullText;
         }
 
         public string ReadFile()
--- FileManipulation/Program.cs
+++ FileManipulation/Program.cs
@@ -1,4 +1,5 @@
-﻿using FileManipulation;
+﻿using System.Net;
+using FileManipulation;
 
 internal class Program
 {
@@ -9,15 +10,50 @@ internal class Program
 
         FileManipulator fm = new(fileName);
 
+        int opt = 0;
         do
         {
-            Console.Clear();
-            fm.WritePerson(CreatePerson());
-            Console.WriteLine("Tecle C para acrescentar mais alguém");
-        } while (Console.ReadLine().ToLower() == "c");
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1 - Ler lista");
+                Console.WriteLine("2 - Ler quantidade de linhas");
+                Console.WriteLine("3 - Adicionar pessoa");
+                Console.WriteLine("4 - Sair");
+                int.TryParse(Console.ReadLine(), out opt);
+            } while (opt < 1 || opt > 4);
 
-        Console.WriteLine(fm);
+            if (opt == 4) Environment.Exit(0);
 
+            if (opt == 1) Console.WriteLine(fm);
+            else if (opt == 2)
+            {
+                int range = 0;
+                do
+                {
+                    Console.Clear();
+                    Console.WriteLine("Escolha a quantidade de linhas:");
+                    int.TryParse(Console.ReadLine(), out range);
+                } while (range == 0);
+
+                string[] rangeText = fm.ReadFileLines(range);
+
+                foreach (string line in rangeText)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            else
+            {
+                do
+                {
+                    Console.Clear();
+                    fm.WritePerson(CreatePerson());
+                    Console.WriteLine("Tecle C para acrescentar mais alguém");
+                } while (Console.ReadLine().ToLower() == "c");
+            }
+            Console.WriteLine("Digite S para continuar | Qualquer tecla para finalizar: ");
+        } while (Console.ReadLine().ToUpper() == "S");
     }
 
     private static Person CreatePerson()
